1641d5b48a3ec11add24b15134f0e2d3811f7779
Ariel <arina.kudar@gmail.com>
2025-04-03 21:05:25 +0300

    push header

---
+++ src/js/header.js
@@ -0,0 +1,28 @@
+const openNavBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav-list");
+const openModalBtn = document.querySelector(".burger-btn");
+const closeModalBtn = document.querySelector(".mobile-close-btn");
+const modal = document.querySelector(".mobile-menu");
+
+openNavBtn.addEventListener('click', toggleNav);
+
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+function toggleNav() {
+    nav.classList.toggle('is-open');
+}
+
+function openModal() {
+    modal.classList.add("is-open");
+}
+
+function closeModal() {
+    modal.classList.remove("is-open");
+
+}
