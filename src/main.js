3a95b6ac13401916e59cd12041d94056ffb21ef5
Ariel <arina.kudar@gmail.com>
2025-04-06 21:55:15 +0300

    fix js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './js/header.js';
+
+import './js/projects.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/work-together.js';
+/* 
+import './js/about-me.js';
+
+
+
+
+ */
