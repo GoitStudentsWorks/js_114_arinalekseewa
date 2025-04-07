61a39451cb5767ee157c2556046778094a14ed78
Ariel <arina.kudar@gmail.com>
2025-04-08 00:54:53 +0300

    update about-me

--- src/js/about-me-2.js
+++ src/js/about-me-2.js
@@ -4,6 +4,7 @@ import Swiper from 'swiper';
 import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Accordion
   const accordion = new Accordion('.about-me-accordion', {
     duration: 400,
     showMultiple: true,
@@ -14,7 +15,6 @@ document.addEventListener('DOMContentLoaded', function () {
   items.forEach((item, index) => {
     const content = item.querySelector('div:nth-of-type(2)');
     const arrow = item.querySelector('.icon-arrow');
-
     content.classList.add('about-me-content');
 
     if (index === 0) {
@@ -31,39 +31,36 @@ document.addEventListener('DOMContentLoaded', function () {
       const arrow = item.querySelector('.icon-arrow');
       const isOpen = item.classList.toggle('open');
 
-      if (isOpen) {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      } else {
-        content.style.maxHeight = '0px';
-      }
-
+      content.style.maxHeight = isOpen ? content.scrollHeight + 'px' : '0px';
       arrow.style.transform = isOpen ? 'rotate(180deg)' : 'rotate(0deg)';
     });
   });
-});
 
-const swiper = new Swiper('.about-me-swiper', {
-  loop: true,
-  slidesPerView: 2,
-  modules: [Navigation, Mousewheel, Keyboard],
-  navigation: {
-    nextEl: '.swiper-button-next',
-  },
-  keyboard: { enabled: true, onlyInViewport: false, pageUpDown: true },
-  mousewheel: true,
-  roundLengths: true,
-  breakpoints: {
-    768: { slidesPerView: 3 },
-    1440: { slidesPerView: 6 },
-  },
-});
-swiper.on('slideChange', function () {
-  const activeSlide = swiper.slides[swiper.activeIndex];
-  // Убедитесь, что элемент существует, прежде чем добавлять класс
-  if (activeSlide) {
-    document
-      .querySelectorAll('.about-me-item-skils')
-      .forEach(el => el.classList.remove('active'));
-    activeSlide.classList.add('active');
-  }
+  // Swiper
+  const swiper = new Swiper('.about-me-swiper-container', {
+    loop: false,
+    slidesPerView: 2,
+    modules: [Navigation, Mousewheel, Keyboard],
+    navigation: {
+      nextEl: '.swiper-button-next',
+    },
+    keyboard: { enabled: true, onlyInViewport: false, pageUpDown: true },
+    mousewheel: true,
+    roundLengths: true,
+    breakpoints: {
+      768: { slidesPerView: 3 },
+      1440: { slidesPerView: 6 },
+    },
+  });
+
+  swiper.on('slideChange', function () {
+    document.querySelectorAll('.about-me-item-skils').forEach(el => el.classList.remove('active'));
+
+    const activeIndex = swiper.activeIndex;
+    const slides = swiper.slides;
+
+    if (slides[activeIndex]) {
+      slides[activeIndex].classList.add('active');
+    }
+  });
 });
